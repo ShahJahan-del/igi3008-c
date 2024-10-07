16429d67fb139aa3993e65c83ab358c9f84448b7
SheikhShahJahan <shahjahan.sheikh@edu.esiee.fr>
2024-10-07 13:38:12 +0000

    hello

---
+++ main_Structures_de_controle.c
@@ -0,0 +1,17 @@
+// primes.c
+
+// #include <stdio.h>
+
+// int isprime(int p){
+//     for (int i = 2, i<int p; i++){
+//         if (i%p==0) break
+//     }
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main(){
+    printf("Hello World !");
+    return 0;
+}
